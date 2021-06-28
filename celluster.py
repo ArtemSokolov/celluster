3380c4cd72bf6dc5bfae41ee63ac5c33ac0c0142
Matt Hodgman <matthodg1@gmail.com>
2021-06-28 15:42:23 -0600

    implemented markers arg

--- celluster.py
+++ celluster.py
@@ -10,8 +10,24 @@ def parseArgs():
     parser.add_argument('-i', '--input', help="Input CSV of mcmicro marker expression data for cells", type=str, required=True)
     parser.add_argument('-o', '--output', help='The directory to which output files will be saved', type=str, required=False)
     parser.add_argument('-m', '--markers', help='A text file with a marker on each line to specify which markers to use for clustering', type=str, required=False)
+    parser.add_argument('-v', '--verbose', help='Flag to print out progress of script', required=False)
     args = parser.parse_args()
     return args
+
+
+'''
+Get markers to use for clustering from a text file where each marker is on a line and corresponds exactly to the column name in the input data file.
+Returns a list of markers to use for clustering.
+'''
+def get_markers(markers_file):
+    markers = [] # list of markers in file
+
+    # read markers from file
+    f = open(markers_file, 'r')
+    for line in f:
+        markers.append(line)
+
+    return markers
 
 
 '''
@@ -30,10 +46,8 @@ Exclude the following data from clustering:
 To include any of these markers in the clustering, provide their exact names in a file passed in with the '-m' flag
 '''
 def clean(input_file):
-    print('Cleaning dataset...')
 
-    # load csv
-    data = pd.read_csv(input_file)
+    # constants
 
     # a default list of features to exclude from clustering
     FEATURES_TO_REMOVE = ['X_centroid', 'Y_centroid', # morphological features
@@ -45,28 +59,43 @@ def clean(input_file):
                         'AF', # autofluorescence
                         'A'] # secondary antibody staining only
 
-    # find any columns in the input csv that should be excluded from clustering be default
-    # NOTE: may want to replace this with regex, it might be faster.
-    col_to_remove = []
-    for col in data.columns:
-        for feature in FEATURES_TO_REMOVE:
-            if col.startswith(feature):
-                col_to_remove.append(col)
-    
-    # drop all columns that should be excluded
-    data = data.drop(columns=col_to_remove, axis=1)
+    CELL_ID = 'CellID' # column name holding cell IDs
+
+    if args.verbose:
+        print('Cleaning data...')
+
+    # load csv
+    data = pd.read_csv(input_file)
+
+    # if markers provided, keep only those features and the Cell IDs. It is important that the CellID column is first.
+    if args.markers:
+        markers.insert(0, CELL_ID)
+        data = data[markers]
+    else:
+        # find any columns in the input csv that should be excluded from clustering be default
+        # NOTE: may want to replace this with regex, it might be faster.
+        col_to_remove = []
+        for col in data.columns:
+            for feature in FEATURES_TO_REMOVE:
+                if col.startswith(feature):
+                    col_to_remove.append(col)
+        
+        # drop all columns that should be excluded
+        data = data.drop(columns=col_to_remove, axis=1)
 
     # save cleaned data to csv
     data.to_csv(f'{output}/clean_data.csv', index=False)
 
-    print('Done.')
+    if args.verbose:
+        print(f'Done. Cleaned data is in {output}/clean_data.csv.')
 
 
 '''
 Run an R script that runs FastPG. Scriptception.
 '''
 def runFastPG():
-    print('Running R script...')
+    if args.verbose:
+        print('Running R script...')
     import subprocess
 
     r_script = ['Rscript', 'celluster/runFastPG.r'] # use FastPG.r script
@@ -78,8 +107,9 @@ def runFastPG():
     # run R script and get modularity from stdout 
     modularity = subprocess.check_output(command, universal_newlines=True)
 
-    print(f'Modularity: {modularity}')
-    print('Done.')
+    if args.verbose:
+        print(f'Modularity: {modularity}')
+        print('Done.')
 
 
 '''
@@ -87,7 +117,17 @@ Main.
 '''
 if __name__ == '__main__':
     args = parseArgs() # parse arguments
+
+    # get user-defined output dir or set to current
     if args.output is None:
         output = '.'
+
+    # get list of markers if provided
+    if args.markers is not None:
+        markers = get_markers(args.markers)
+    
+    # clean input data file
     clean(args.input)
+
+    # run FastPG algorithm
     runFastPG()
