9ef5743dc6418003f9bea0d8b95374751d750ca1
Matt Hodgman <matthodg1@gmail.com>
2021-06-28 13:59:38 -0600

    make cluster.csv

--- runFastPG.r
+++ runFastPG.r
@@ -1,11 +1,15 @@
+# get data and cluster it
 args <- commandArgs(trailingOnly=TRUE) # required command line arguments order: {cleaned data csv} {k}
 data <- as.matrix(read.csv(file=args[1])) # load cleaned data into matrix
 clusters <- FastPG::fastCluster(data=data, k=args[2]) # compute clusters
-cellID <- data[,1] # get all cell IDs
 community <- clusters$communities # get all cell community assignations (these are in the same order as cells in data)
-cells <- cbind(cellID, community) # combine cell IDs and community assignations for export
-write.table(cells,file="cells.csv",row.names=FALSE,quote=FALSE) # write data to csv
+data <- cbind(community, data) # add community assignation to data
 
-# TO DO: make csv with cluster identifications
+# make cells.csv
+cells <- (data[,c('CellID','community')]) # get just cell IDs and community assignations for export
+write.table(cells,file="cells.csv",row.names=FALSE,quote=FALSE,sep=',') # write data to csv
 
-cat(clusters$modularity)
+# make clusters.csv
+clusterData <- aggregate(data[,-1], list(data[,'community']), mean) # group feature/expression data by cluster and find mean expression for each cluster
+cols <- cols[cols != 'Group.1'] # remove group number because is identical to community assignation number
+write.table(clusterData[,cols],file="clusters.csv",row.names=FALSE,quote=FALSE,sep=',') # write data to csv
