00e4794f64e94ebbf306a1142f531ebc5cc61d84
Matt Hodgman <matthodg1@gmail.com>
2021-07-02 09:05:42 -0600

    improved commenting

--- runFastPG.r
+++ runFastPG.r
@@ -1,4 +1,4 @@
-# runFastPG.r runs the FastPG program (https://github.com/sararselitsky/FastPG) to cluster cells by the markers
+# runFastPG.r runs the FastPG program (https://github.com/sararselitsky/FastPG), an R implementation of the Phenograph method, to cluster cells by the markers
 # included in the input csv.
 #
 # Arguments:
@@ -13,18 +13,18 @@
 
 
 # get data and cluster it
-args <- commandArgs(trailingOnly=TRUE) # required command line arguments order: {cleaned data csv} {k}
-data <- as.matrix(read.csv(file=args[1])) # load cleaned data into matrix TIME: 273.236s
-clusters <- FastPG::fastCluster(data=data, k=as.integer(args[2]), num_threads=as.integer(args[3])) # compute clusters TIME: 144.232s
-community <- clusters$communities # get all cell community assignations (these are in the same order as cells in data) TIME: 0s
-data <- cbind(community, data) # add community assignation to data TIME: 0.504s
+args <- commandArgs(trailingOnly=TRUE) # required command line arguments order: {cleaned data csv} {k} {num_threads} {output dir}
+data <- as.matrix(read.csv(file=args[1])) # load cleaned data into matrix
+clusters <- FastPG::fastCluster(data=data, k=as.integer(args[2]), num_threads=as.integer(args[3])) # compute clusters
+community <- clusters$communities # get all cell community assignations (these are in the same order as cells in data)
+data <- cbind(community, data) # add community assignation to data
 
 # make cells.csv
-cells <- (data[,c('CellID','community')]) # get just cell IDs and community assignations for export TIME: 0.005s
-write.table(cells,file=paste(args[4],'cells.csv', sep='/'),row.names=FALSE,quote=FALSE,sep=',') # write data to csv TIME: 3.478s
+cells <- (data[,c('CellID','community')]) # get just cell IDs and community assignations for export
+write.table(cells,file=paste(args[4],'cells.csv', sep='/'),row.names=FALSE,quote=FALSE,sep=',') # write data to csv
 
 # make clusters.csv
-clusterData <- aggregate(subset(data, select=-c(CellID)), list(data[,'community']), mean) # group feature/expression data by cluster and find mean expression for each cluster, remove CellID column TIME: 6.780s
-write.table(subset(clusterData, select=-c(Group.1)),file=paste(args[4],'clusters.csv', sep='/'),row.names=FALSE,quote=FALSE,sep=',') # remove group number column because is identical to community assignation number and write data to csv TIME: 0.037s
+clusterData <- aggregate(subset(data, select=-c(CellID)), list(data[,'community']), mean) # group feature/expression data by cluster and find mean expression for each cluster, remove CellID column
+write.table(subset(clusterData, select=-c(Group.1)),file=paste(args[4],'clusters.csv', sep='/'),row.names=FALSE,quote=FALSE,sep=',') # remove group number column because is identical to community assignation number and write data to csv
 
 cat(clusters$modularity) # output modularity
