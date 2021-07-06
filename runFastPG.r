7645e01cf82b8bac33ef111e01788debac209daf
Matt Hodgman <matthodg1@gmail.com>
2021-07-06 16:01:35 -0600

    renamed 'community' column to 'Cluster'

--- runFastPG.r
+++ runFastPG.r
@@ -18,15 +18,15 @@
 args <- commandArgs(trailingOnly=TRUE) # required command line arguments order: {cleaned data csv} {k} {num_threads} {output dir}
 data <- as.matrix(read.csv(file=args[1])) # load cleaned data into matrix
 clusters <- FastPG::fastCluster(data=data, k=as.integer(args[2]), num_threads=as.integer(args[3])) # compute clusters
-community <- clusters$communities # get all cell community assignations (these are in the same order as cells in data)
-data <- cbind(community, data) # add community assignation to data
+Cluster <- clusters$communities # get all cell community assignations (these are in the same order as cells in data)
+data <- cbind(Cluster, data) # add community assignation to data
 
 # make cells.csv
-cells <- (data[,c('CellID','community')]) # get just cell IDs and community assignations for export
+cells <- (data[,c('CellID','Cluster')]) # get just cell IDs and community assignations for export
 write.table(cells,file=paste(args[4], args[5], sep='/'),row.names=FALSE,quote=FALSE,sep=',') # write data to csv
 
 # make clusters.csv
-clusterData <- aggregate(subset(data, select=-c(CellID)), list(data[,'community']), mean) # group feature/expression data by cluster and find mean expression for each cluster, remove CellID column
+clusterData <- aggregate(subset(data, select=-c(CellID)), list(data[,'Cluster']), mean) # group feature/expression data by cluster and find mean expression for each cluster, remove CellID column
 write.table(subset(clusterData, select=-c(Group.1)),file=paste(args[4], args[6], sep='/'),row.names=FALSE,quote=FALSE,sep=',') # remove group number column because is identical to community assignation number and write data to csv
 
 cat(clusters$modularity) # output modularity
